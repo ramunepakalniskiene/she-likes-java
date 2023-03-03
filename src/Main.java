92a6573f0cb248eca2e460b6492b18bce9980c98
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-03 21:28:03 +0200

    introducing other data types: double, boolean, String

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,23 @@ public class Main {
         int myDaugtherAge = 11;
         myDaugtherAge=12;
         System.out.println(myDaugtherAge);
+
+        String myName="Ramune";
+        String mySurName="Pakalniskiene";
+        System.out.println(myName + "  " + mySurName);
+
+        String five = "5";
+        String otherFive = "5";
+        System.out.println(five + otherFive);
+
+        // true false
+        boolean trueValue = true;
+        trueValue=false;
+        System.out.println(trueValue);
+
+        //11.0
+        int eleven = 11;
+        double trueEleven = 11;
+        System.out.println(eleven +  " + " + 11);
     }
 }

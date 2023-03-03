3dbf0f6cf728f92269a737507e0f06538ad3a76a
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-03 21:06:37 +0200

    adding variable MyDaugther

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int myDaugtherAge = 11;
+        myDaugtherAge=12;
+        System.out.println(myDaugtherAge);
     }
 }

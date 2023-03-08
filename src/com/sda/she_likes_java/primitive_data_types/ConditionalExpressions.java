6404b398ab0e13f1d021483f2aae9a4bdf0e31bf
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-08 21:29:40 +0200

    conditional statement if/else

--- src/com/sda/she_likes_java/primitive_data_types/ConditionalExpressions.java
+++ src/com/sda/she_likes_java/primitive_data_types/ConditionalExpressions.java
@@ -1,5 +1,7 @@
 package com.sda.she_likes_java.primitive_data_types;
 
+import java.util.Scanner;
+
 public class ConditionalExpressions {
     public static void main(String[] args) {
         System.out.println("Conditionals :");
@@ -13,6 +15,17 @@ public class ConditionalExpressions {
             System.out.println("This will be run only when condition is true");
         }else{
             System.out.println("This will be run when condition is false");
+        }
+        System.out.println();
+
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Tell me how old are you? ");
+        int personAge = inputReader.nextInt();
+
+        if(personAge>=18) {
+            System.out.println("Hello lady!");
+        } else{
+            System.out.println("It's time to go to sleep, Kid");
         }
     }
 }

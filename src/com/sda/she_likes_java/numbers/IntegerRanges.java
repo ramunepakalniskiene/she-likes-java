63d2d2f1a687c0fb0a07ba9ceec28f52ab90b38f
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-08 20:36:09 +0200

    ranges of int numbers

---
+++ src/com/sda/she_likes_java/numbers/IntegerRanges.java
@@ -0,0 +1,30 @@
+package com.sda.she_likes_java.numbers;
+
+public class IntegerRanges {
+    public static void main(String[] args) {
+        System.out.println("Let's see how integer ranges looks like");
+        System.out.println("\nint ranges:");
+        System.out.println("minimal int number is: " + Integer.MIN_VALUE);
+        System.out.println("maximal int number is: " + Integer.MAX_VALUE);
+
+        System.out.println("------------------------");
+
+        System.out.println("Let's check how long type looks like");
+        System.out.println("minimal long number is: " + Long.MIN_VALUE);
+        System.out.println("maximal long value is: " + Long.MAX_VALUE);
+
+        System.out.println("-------------------------");
+
+        System.out.println("Let's check how short type looks like"); // two time less numbers than long type
+        System.out.println("minimal short value is: " + Short.MIN_VALUE);
+        System.out.println("maximal short value is: " + Short.MAX_VALUE);
+
+        System.out.println("-------------------------");
+
+        System.out.println("Let's check how byte type looks like");
+        System.out.println("minimal byte number is: " + Byte.MIN_VALUE);
+        System.out.println("maximal byte value is: " + Byte.MAX_VALUE);
+
+        System.out.println("-------------------------");
+    }
+}

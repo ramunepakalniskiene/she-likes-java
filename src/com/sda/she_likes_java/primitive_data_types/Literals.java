19dbb27e52c58fd42b8e49f4fb034650cf1976ea
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-10 21:19:10 +0200

    literals

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is direct value
+        //String literal is "name"
+        String name = "Mariusz";
+
+        // integer literal
+        int age = 40; // 40 is type of int
+
+        // long literal
+        long anotherAge = 30L;
+
+        // double literal
+        double doubleNumber=3.1;
+
+        //float literal;
+        float floatNumber=3.1F;
+    }
+}

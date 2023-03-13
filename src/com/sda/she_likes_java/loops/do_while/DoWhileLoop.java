012d9a6365896a5fbfe142bfe783b2b880daae91
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-13 20:46:33 +0200

    do while first example

---
+++ src/com/sda/she_likes_java/loops/do_while/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.loops.do_while;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        boolean isAdult = true;
+
+        do {
+            System.out.println("Can you se this line");
+
+        } while (isAdult);
+
+    }
+}

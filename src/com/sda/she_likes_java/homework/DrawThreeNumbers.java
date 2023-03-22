13ca7a58a5205947f133a69a2effdecceab6840a
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-22 20:45:17 +0200

    Random generator  - homework

---
+++ src/com/sda/she_likes_java/homework/DrawThreeNumbers.java
@@ -0,0 +1,28 @@
+package com.sda.she_likes_java.homework;
+
+import javax.sound.midi.Soundbank;
+import java.util.Random;
+
+public class DrawThreeNumbers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int firstNumber = random.nextInt(10);
+        int secondNumber = random.nextInt(10);
+        int thirdNumber = random.nextInt(10);
+
+        int minNumber;
+        int middleNumber;
+        int maxNumber;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNumber);
+        if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+//            firstNumber=maxNumber;
+//
+//        }if(){
+//
+//        }
+        }
+    }
+}

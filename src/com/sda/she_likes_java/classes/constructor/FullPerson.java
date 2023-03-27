52403fbf19ce95fb46b20cb2ff6365cb97674fa6
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-27 21:11:19 +0300

    static method first touch

--- src/com/sda/she_likes_java/classes/constructor/FullPerson.java
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -27,11 +27,21 @@ public class FullPerson {
 
 
     }
-
+    public void describeYourself(){
+        System.out.println("I'm " + name);
+    }
+    public static void genericDescription(){
+        System.out.println("Hi I'm ");
+    }
     public static void main(String[] args) {
        FullAddress address = new FullAddress("Lithuania", "Vilnius", "Sanatariskiu", "90865");
        FullPerson person=new FullPerson("Rasa", "Rasutyte", 45,address);
 
         System.out.println("Person is: "+ person);
+        person.describeYourself();
+        FullPerson personWithoutAddress = new FullPerson("Tomas", "Tomasauskas", 50, null);
+        personWithoutAddress.describeYourself();
+        System.out.println("Person without address: " + personWithoutAddress);
+        genericDescription();
     }
 }

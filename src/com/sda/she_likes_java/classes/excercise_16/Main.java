1a6fd6f284da8b361d5f397f5163d71459ccf965
Ramune <ramune.pakalniskiene@gmail.com>
2023-03-31 18:38:28 +0300

    Homework excercise_16

--- src/com/sda/she_likes_java/classes/excercise_16/Main.java
+++ src/com/sda/she_likes_java/classes/excercise_16/Main.java
@@ -2,10 +2,11 @@ package com.sda.she_likes_java.classes.excercise_16;
 
 public class Main {
     public static void main(String[] args) {
-        Owner owner=new Owner("Roger", "Freeman",new Animal());
         Animal dog=new Animal("Rex", 4,"terrier");
+        Owner John=new Owner("Roger", "Freeman",dog);
 
-        System.out.println(owner);
-        System.out.println(dog);
+
+        System.out.println("The owner" + John );
+
     }
 }

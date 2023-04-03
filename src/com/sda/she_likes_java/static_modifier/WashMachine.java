06ee96440eb0339c85f620974a58898701e0e3c8
Ramune <ramune.pakalniskiene@gmail.com>
2023-04-03 20:58:53 +0300

    static example with - washMachine class

---
+++ src/com/sda/she_likes_java/static_modifier/WashMachine.java
@@ -0,0 +1,50 @@
+package com.sda.she_likes_java.static_modifier;
+
+public class WashMachine {
+    private String brand;
+    private String model;
+
+    private int serialNumber;
+
+    private static int numberOfCreatedMachines;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public WashMachine(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        numberOfCreatedMachines++;
+        serialNumber++;
+
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public static int getNumberOfCreatedMachines() {
+        return numberOfCreatedMachines;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public static void setNumberOfCreatedMachines(int numberOfCreatedMachines) {
+        WashMachine.numberOfCreatedMachines = numberOfCreatedMachines;
+    }
+
+    @Override
+    public String toString() {
+        return "WashMachine{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
---
+++ src/com/sda/she_likes_java/static_modifier/WashMachineFactory.java
@@ -0,0 +1,52 @@
+package com.sda.she_likes_java.static_modifier;
+
+public class WashMachineFactory {
+    public static void main(String[] args) {
+        System.out.println("Current number of produced machines is: " + WashMachine.getNumberOfCreatedMachines());
+        System.out.println("=======================================================");
+        WashMachine samsung= new WashMachine("Samsung", "1x2");
+        System.out.println("Samsung info: " + samsung);
+        System.out.println("Current quantity of made washmachines is:" + WashMachine.getNumberOfCreatedMachines());
+        System.out.println("Samsung serial number is: " + samsung.getSerialNumber());
+        samsung.setSerialNumber(5);
+        System.out.println("Samsung serial number is: " + samsung.getSerialNumber());
+        //Do not use instance reference to access static members(fields/methods);
+        samsung.setNumberOfCreatedMachines(1000);
+        System.out.println("=========================================================");
+
+        WashMachine miele =new WashMachine("Miele", "ML023");
+        System.out.println("Miele info: " + miele);
+        System.out.println("Current quantity of made washmachines is:" + WashMachine.getNumberOfCreatedMachines());
+        System.out.println("Miele serial number is: " + miele.getSerialNumber());
+        miele.setSerialNumber(4);
+        System.out.println("Miele serial number is: " + miele.getSerialNumber());
+        System.out.println("=========================================================");
+
+        WashMachine lg =new WashMachine("LG", "00000");
+        System.out.println("LG info: " + lg);
+        System.out.println("Current quantity of made washmachines is:" + WashMachine.getNumberOfCreatedMachines());
+        System.out.println("Miele serial number is: " + lg.getSerialNumber());
+        System.out.println("=========================================================");
+
+
+        for(int i=0;i<10; i++){
+        makeSomeWashMachines();
+        }
+        System.out.println("Hey, we have totaly produced: " + WashMachine.getNumberOfCreatedMachines());
+        // some generic way to obtain number of produced machines
+
+
+    }
+    public static void makeSomeWashMachines(){
+        new WashMachine("LG", "1111000");
+        new WashMachine("LG", "1111111");
+        new WashMachine("LG", "1111222");
+        new WashMachine("LG", "1111333");
+        new WashMachine("LG", "1111444");
+        new WashMachine("LG", "1111555");
+
+
+    }
+
+
+}

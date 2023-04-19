1856e370cde5857f668c415fefcd8679f908a5bb
Ramune <ramune.pakalniskiene@gmail.com>
2023-04-19 19:27:36 +0300

    excercise_updated object names

--- src/com/sda/she_likes_java/homework/excercise_17/BookMain.java
+++ src/com/sda/she_likes_java/homework/excercise_17/BookMain.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class BookMain {
     public static void main(String[] args) {
-        Book first = new Book("Stendal", "Red&Black", 1830);
-        Book second = new Book("Balzac", "Father_Gorijot", 1835);
-        Book third = new Book("Mark_Twain", "Tom_Sojer", 1876);
+        Book firstBook = new Book("Stendal", "Red&Black", 1830);
+        Book secondBook = new Book("Balzac", "Father_Gorijot", 1835);
+        Book thirdBook = new Book("Mark_Twain", "Tom_Sojer", 1876);
 
         Random random = new Random();
     }

1158b0eb2f5f43044d77dc3d1227731a2c0ae740
Ramune <ramune.pakalniskiene@gmail.com>
2023-04-24 18:53:52 +0300

    Excercise_19 Vehicle created classes with inheritance

---
+++ src/com/sda/she_likes_java/homework/excercise_19/Main.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.homework.excercise_19;
+
+public class Main {
+    Vehicle ship = new Ship();
+    Vehicle plane = new Plane();
+
+
+}
--- src/com/sda/she_likes_java/homework/excercise_19/Vehicle.java
+++ src/com/sda/she_likes_java/homework/excercise_19/Vehicle.java
@@ -2,5 +2,6 @@ package com.sda.she_likes_java.homework.excercise_19;
 
 public class Vehicle {
     public void move() {
+        System.out.println("The vehicle is moving");
     }
 }

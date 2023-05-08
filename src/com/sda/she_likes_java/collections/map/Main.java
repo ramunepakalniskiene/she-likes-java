976bb396d734d6d66854153432de6dd344c04cef
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-08 21:31:16 +0300

    map example: EntrySet usage

--- src/com/sda/she_likes_java/collections/map/Main.java
+++ src/com/sda/she_likes_java/collections/map/Main.java
@@ -19,7 +19,27 @@ public class Main {
         Map<Person, Trousers> personsTrousers = new HashMap<>();
         personsTrousers.put(new Person("Ra", "P"), new Trousers("polish-material", "Pepco"));
         personsTrousers.put(new Person("Tomasz", "Markus"), new Trousers("jeans", "newyorker"));
+        personsTrousers.put(new Person("Zimmer", "Call"), new Trousers("jeans", "big star"));
 
         System.out.println(personsTrousers);
+        printMyJeans(myJeans);
+    }
+
+    public static void printMyJeans(Map<String, Trousers> trousersMap) {
+        System.out.println("Let's start with known keys");
+        System.out.println("key=[my favorite jeans]" + trousersMap.get("My favorite jeans"));
+        System.out.println("key=[Stupid key]" + trousersMap.get("stupid key"));
+
+        System.out.println("================");
+
+        System.out.println("Let's iterate the map");
+//        Map.Entry represent key + value at once
+        for (Map.Entry<String, Trousers> item : trousersMap.entrySet()) {
+            System.out.println("Key: " + item.getKey());
+            System.out.println("Value: " + item.getValue());
+
+
+        }
+
     }
 }

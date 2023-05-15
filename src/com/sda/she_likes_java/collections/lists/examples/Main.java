76d1ca82a59fc5a0fb37956b0961d8b6e93ce31e
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-15 12:38:29 +0300

    lists example, enhanced loop

--- src/com/sda/she_likes_java/collections/lists/examples/Main.java
+++ src/com/sda/she_likes_java/collections/lists/examples/Main.java
@@ -1,11 +1,34 @@
 package com.sda.she_likes_java.collections.lists.examples;
 
+import java.io.Console;
+import java.util.Scanner;
+
 import static com.sda.she_likes_java.collections.lists.examples.MyDictionaries.createEnglishLithuanianDictionary;
 
 public class Main {
     public static void main(String[] args) {
-        MyGreatDictionary englishLithuanianDictionary = MyDictionaries.createEnglishLithuanianDictionary();
-        englishLithuanianDictionary.translateFromSourceLanguageToDestiny("dog");
-        englishLithuanianDictionary.translateFromSourceLanguageToDestiny("man");
+        System.out.println("Hi, which dictionary you'd like to use");
+        System.out.println("type 1 for English - Lithuanian");
+        System.out.println("type 2 for English - Russian");
+//        Console console = System.console();
+//        String userChoice = console.readLine();
+        Scanner inputReader = new Scanner(System.in);
+        String userChoice = inputReader.next();
+
+        MyGreatDictionary currentDictionary;
+        if (userChoice.equals("1")) {
+            currentDictionary = MyDictionaries.createEnglishLithuanianDictionary();
+            System.out.println("let's use English - Lithuanian dictionary");
+        } else if (userChoice.equals("2")) {
+            currentDictionary = MyDictionaries.createEnglishRussianDictionary();
+            System.out.println("let's use English - Russian dictionary");
+        } else {
+            System.out.println("I don't know what you mean");
+            return;
+        }
+
+        System.out.println("Provide word to translate: ");
+        String wordFromTheUser = inputReader.next();
+        currentDictionary.translateFromSourceLanguageToDestiny(wordFromTheUser);
     }
 }

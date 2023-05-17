26cd96c8c5979ec711e0320d8f9c4dd0d819e8e6
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-17 20:13:40 +0300

    Homework_25

--- src/com/sda/she_likes_java/homework/excercise_25/Main.java
+++ src/com/sda/she_likes_java/homework/excercise_25/Main.java
@@ -3,7 +3,7 @@ package com.sda.she_likes_java.homework.excercise_25;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {

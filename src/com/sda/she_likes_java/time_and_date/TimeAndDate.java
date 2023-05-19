7b578caad7fa7507ede887b4bcd805047f7eea7c
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-19 19:15:29 +0300

    Local Date usage

--- src/com/sda/she_likes_java/time_and_date/TimeAndDate.java
+++ src/com/sda/she_likes_java/time_and_date/TimeAndDate.java
@@ -3,6 +3,7 @@ package com.sda.she_likes_java.time_and_date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class TimeAndDate {
     public static void main(String[] args) {
@@ -17,10 +18,21 @@ public class TimeAndDate {
 
         System.out.println("---------");
 
-        LocalDateTime currentDateAndTime= LocalDateTime.now();
+        LocalDateTime currentDateAndTime = LocalDateTime.now();
         System.out.println("Current date and time in my location is " + currentDateAndTime);
 
         System.out.println("Let's format  a little bit our date and time");
         System.out.println(currentDateAndTime.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+//      Do not use Date clas it is - deprecated Date date = new Date();
+        LocalDate now = LocalDate.now();
+        LocalDate future = now.plus(5, ChronoUnit.DAYS);
+        System.out.println(now);
+        System.out.println(future);
+        LocalDate previousYear = now.minusYears(1);
+        System.out.println(previousYear);
+        Boolean isBefore = previousYear.isBefore(now);
+        System.out.println(isBefore);
+
     }
 }

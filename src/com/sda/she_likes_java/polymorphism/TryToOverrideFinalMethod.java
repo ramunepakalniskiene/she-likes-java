671e5c0a2d1ce56bd26ba09823e6aceed483f0c7
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-22 19:19:31 +0300

    final class and methods

---
+++ src/com/sda/she_likes_java/polymorphism/TryToOverrideFinalMethod.java
@@ -0,0 +1,6 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class TryToOverrideFinalMethod
+        extends WithFinalMethod {
+
+}
---
+++ src/com/sda/she_likes_java/polymorphism/WithFinalMethod.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class WithFinalMethod {
+
+    public final void notToOverride() {
+
+    }
+}

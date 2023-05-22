18f1987d64351c71ec65188acf5fce5ecd1ca983
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-22 19:19:20 +0300

    final class and methods

---
+++ src/com/sda/she_likes_java/polymorphism/FinalCat.java
@@ -0,0 +1,13 @@
+package com.sda.she_likes_java.polymorphism;
+
+public final class FinalCat {
+    private String name;
+
+    public FinalCat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

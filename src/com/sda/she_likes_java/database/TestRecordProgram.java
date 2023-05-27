8ec69e634ef503c78b43d68d5ec4e5e635b80737
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-27 10:52:13 +0300

    adding h2 jar

--- src/com/sda/she_likes_java/database/TestRecordProgram.java
+++ src/com/sda/she_likes_java/database/TestRecordProgram.java
@@ -7,7 +7,7 @@ import static com.sda.she_likes_java.database.DatabaseUtils.*;
 public class TestRecordProgram {
     public static void main(String[] args) {
         Connection connection = getConnection();
-        System.out.println(" Got connection " + (connection != null));
+        System.out.println(" Got connection? " + (connection != null));
         TestRecordRepository recordRepository = new TestRecordRepository(connection);
         recordRepository.getAllTestRowsFromDB();
 

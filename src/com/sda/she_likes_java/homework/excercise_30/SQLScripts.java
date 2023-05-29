2fa643cacc3d0a9800b4c006835f628806c9c748
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-29 12:05:11 +0300

    SQL scripts homework

---
+++ src/com/sda/she_likes_java/homework/excercise_30/SQLScripts.java
@@ -0,0 +1,34 @@
+package com.sda.she_likes_java.homework.excercise_30;
+
+public class SQLScripts {
+    /**
+     * CREATE TABLE STUDENTS
+     * (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(255),
+     * SURNAME VARCHAR(255), AGE INT, SEX VARCHAR(255);
+     *
+     * INSERT INTO TABLE STUDENTS (ID, NAME, SURNAME,AGE, SEX)
+     * VALUES
+     * (1, 'John', 'Smith', 45,'man'),
+     * (2, 'Roberta', 'Jones', 24, 'woman'),
+     * (3, 'Melissa', 'Rodrigues', 22,'woman'),
+     * (4, 'Tom', 'Phillips', 19, 'man'),
+     * (5, 'Ben', 'Thomson', 20, 'man'),
+     * (6, 'Catherine',	'Biden', 19, 'woman');
+     *
+     *  CREATE TABLE STUDENTS
+     *  (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(255),
+     *  SURNAME VARCHAR(255), AGE INT, SEX VARCHAR(255);
+     *
+     * SELECT ID, NAME, SURNAME, AGE, SEX
+     * FROM STUDENTS;
+     *
+     * ALTER TABLE STUDENTS ALTER ID  AUTO_INCREMENT=8;
+     *
+     * INSERT INTO STUDENTS (ID,NAME,SURNAME,AGE,SEX)
+     * VALUES
+     * (7,'Joe', 'Blanck', 20,'man');
+     *
+     * DELETE FROM STUDENTS
+     * WHERE ID=;
+     */
+}

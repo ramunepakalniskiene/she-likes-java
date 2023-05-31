a31cd914ce705d796e3f4f62dcda92aa4f13ec92
Ramune <ramune.pakalniskiene@gmail.com>
2023-05-31 21:00:58 +0300

    CRUD repository for AccentureStudent

---
+++ src/com/sda/she_likes_java/database_in_memory/AccentureStudentsRepository.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.database_in_memory;
+
+import java.util.List;
+
+//CRUD means
+//Create
+//Read
+//Update
+//Delete
+public class AccentureStudentsRepository {
+    public List<AccentureStudent> getAllStudents() {
+        return List.of();
+    }
+
+    public AccentureStudent getStudentByID(Long id) {
+        return null;
+    }
+
+    public AccentureStudent saveStudent(AccentureStudent student) {
+        return null;
+    }
+
+    public boolean deleteStudentByID(Long id) {
+        return true;
+    }
+
+    public AccentureStudent updateStudent(AccentureStudent student) {
+        return null;
+    }
+
+}
